c2f556c21bfe5489d75fa564d2c6451dd017a6e1
ArkPogosyan <145668525+ArkPogosyan@users.noreply.github.com>
2024-05-26 11:26:42 +0300

    Add files via upload

---
+++ DZ10_2.py
@@ -0,0 +1,9 @@
+nuevo_dict = {}
+primero_numero = int(input('Введите число начала: '))
+segundo_numero = int(input('введите стмло конца: '))
+if primero_numero > segundo_numero:
+    for i in range(primero_numero, segundo_numero - 1, -1):
+        nuevo_dict[i] = i ** i
+    for i in range(primero_numero, segundo_numero + 1):
+        nuevo_dict[i] = i ** i
+print(nuevo_dict)
